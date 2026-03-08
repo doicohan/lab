37007988e038b68d9f2e172cfdcde4d11dbd771d
Đông <dongp1901@gmail.com>
2026-03-09 00:49:29 +0700

    Add files via upload

---
+++ TaskManagement/src/main/java/entity/Task.java
@@ -0,0 +1,66 @@
+package entity;
+
+public class Task {
+
+    private int id;
+    private String requirementName;
+    private int taskType;
+    private String date;
+    private double planFrom;
+    private double planTo;
+    private String assignee;
+    private String reviewer;
+
+    public Task() {
+    }
+
+    public Task(int id, String requirementName, int taskType, String date,
+            double planFrom, double planTo, String assignee, String reviewer) {
+
+        this.id = id;
+        this.requirementName = requirementName;
+        this.taskType = taskType;
+        this.date = date;
+        this.planFrom = planFrom;
+        this.planTo = planTo;
+        this.assignee = assignee;
+        this.reviewer = reviewer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getRequirementName() {
+        return requirementName;
+    }
+
+    public int getTaskType() {
+        return taskType;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public double getPlanFrom() {
+        return planFrom;
+    }
+
+    public double getPlanTo() {
+        return planTo;
+    }
+
+    public String getAssignee() {
+        return assignee;
+    }
+
+    public String getReviewer() {
+        return reviewer;
+    }
+
+    public double getTime() {
+        return planTo - planFrom;
+    }
+
+}
---
+++ TaskManagement/src/main/java/entity/TaskType.java
@@ -0,0 +1,12 @@
+package entity;
+
+public class TaskType {
+
+    public static final String[] TYPE = {
+        "Code",
+        "Test",
+        "Design",
+        "Review"
+    };
+
+}
